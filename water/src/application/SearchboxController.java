5e06aeea1c6455efd5b3dc38c987c39932685499
SeoungHyuckJeon <sdkazx2717@gmail.com>
2019-12-05 21:03:52 +0900

    Table clear and MouseDoubleClicked Event write

--- water/src/application/SearchboxController.java
+++ water/src/application/SearchboxController.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.ArrayList;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
@@ -11,12 +13,15 @@ import org.w3c.dom.NodeList;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 public class SearchboxController {
 	static getAPIData getAPIData = new getAPIData();
@@ -30,17 +35,22 @@ public class SearchboxController {
 	private TableColumn<wppModel, String> column_Address;
 	@FXML
 	private TableColumn<wppModel, String> column_Plant;
+	private ArrayList<String> fcltyMngNo = new ArrayList<String>();
 	
 	ObservableList<wppModel> wppList = FXCollections.observableArrayList();
 	
 	public void ClickedbtnSearch() {		//검색 버튼 클리시
 		System.out.println("btn Pressed");
+		wppList.clear();				//테이블 내용 초기화
+		fcltyMngNo.clear();				//배열 초기화
 		getPlantData(main.URL_supplylgld);
 		initialize();
 	}
 	
 	public void PressedSearchBox(KeyEvent event) {		//TextField에서 엔터눌렀을때
-		if(event.getCode() == KeyCode.ENTER) {
+		if(event.getCode() == KeyCode.ENTER) {		//ENTER입력시
+			wppList.clear();			//테이블 내용 초기화
+			fcltyMngNo.clear();			//배열 초기화
 			getPlantData(main.URL_supplylgld);
 			initialize();
 		}
@@ -66,12 +76,7 @@ public class SearchboxController {
 					{
 						wppList.add(new wppModel(new SimpleStringProperty(application.getAPIData.getTagValue("lgldFullAddr", eElement)),
     							new SimpleStringProperty(application.getAPIData.getTagValue("fcltyMngNm", eElement))));
-						
-//						System.out.println("######################");
-						//System.out.println(textfield_SearchBox.getText());
-//						System.out.println("정수장  : " + application.getAPIData.getTagValue("fcltyMngNm", eElement));
-						//System.out.println("정수장번호 : " + getTagValue("fcltyMngNo", eElement));
-//						System.out.println("주소  : " + application.getAPIData.getTagValue("lgldFullAddr", eElement));
+						fcltyMngNo.add(application.getAPIData.getTagValue("fcltyMngNo", eElement));
 					}
 				}
         	}     	
@@ -79,9 +84,15 @@ public class SearchboxController {
         	e.printStackTrace(); }
 	}
 	
-	public void initialize() {
+	public void initialize() {			//불러온값을 각각 셀에 추가하고 셀을 테이블에 추가한다.
 		column_Address.setCellValueFactory(cellData -> cellData.getValue().addressProperty());
 		column_Plant.setCellValueFactory(cellData->cellData.getValue().plantProperty());
 		tableview_WPP.setItems(wppList);
+	}
+	
+	public void CellDoubleClick(MouseEvent event) {
+		if(event.getClickCount()>1 && tableview_WPP.getSelectionModel().getSelectedIndex()!=-1) {	//셀이 선택되어있고, 더블클릭했을때 이벤트
+			System.out.println(tableview_WPP.getSelectionModel().getSelectedIndex());
+		}
 	}
 }
