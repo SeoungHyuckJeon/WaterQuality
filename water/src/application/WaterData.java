57ce712e4ac386e868974b0da792b0089a7e5d05
SeoungHyuckJeon <sdkazx2717@gmail.com>
2019-12-07 13:46:42 +0900

    WaterData에 상세정보로 넘겨줄 변수추가

--- water/src/application/WaterData.java
+++ water/src/application/WaterData.java
@@ -4,7 +4,9 @@ public class WaterData {
 		public static String fcltyMngNm = "정수장을 선택해주십시오.";
 		public static String fcltyAddr = "정수장 주소";
 		public static String date = "'수질정보측정시간' 기준";
-		public static String clVal = "0";
-		public static String phVal = "0";
-		public static String tbVal = "0";
+		public static String clVal = null;				//잔류염소
+		public static String phVal = null;				//pH
+		public static String tbVal = null;				//탁도
+		public static String liIndDivName = "생활정수";	//용수 구분명
+		public static String fcltyMngNo = null;			//시설관리번호
 }
