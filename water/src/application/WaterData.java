9fcb5d63fee72000827d8d6f7e8ccc16d5f6ff0d
SeoungHyuckJeon <sdkazx2717@gmail.com>
2019-12-18 15:21:01 +0900

    디자인 수정

--- water/src/application/WaterData.java
+++ water/src/application/WaterData.java
@@ -3,7 +3,7 @@ package application;
 import javafx.scene.image.Image;
 
 public class WaterData {
-		public static String fcltyMngNm = "정수장을 선택해주십시오.";
+		public static String fcltyMngNm = "여기를 눌러주세요.";
 		public static String fcltyAddr = "정수장 주소";
 		public static String date = "'수질정보측정시간' 기준";
 		public static String clVal = null;				//잔류염소
